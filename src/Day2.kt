862a02927b05adefb4366d3a6f0cc9d4ed27108e
avelaz-android <albertovmo@inditex.com>
2024-12-02 10:56:43 +0100

    Add Day One

---
+++ src/Day2.kt
@@ -0,0 +1,46 @@
+import kotlin.math.abs
+
+fun main() {
+
+    fun isSafe(numbers: List<Int>): Boolean {
+        return numbers.zipWithNext().let { pairs ->
+            if (pairs.any { (a, b) -> abs(a - b) > 3 }) {
+                false
+            } else {
+                val firstDiff = pairs.first().let { (a, b) -> a - b }
+                when {
+                    firstDiff == 0 -> false
+                    firstDiff > 0 -> pairs.all { (a, b) -> a - b > 0 }
+                    else -> pairs.all { (a, b) -> a - b < 0 }
+                }
+            }
+        }
+    }
+
+    fun canBeMadeSafe(numbers: List<Int>): Boolean {
+        return numbers.indices.any { indexToRemove ->
+            val numbersWithoutOne = numbers.filterIndexed { index, _ -> index != indexToRemove }
+            isSafe(numbersWithoutOne)
+        }
+    }
+
+    fun part1(input: List<String>): Int = input.count { line ->
+        val numbers = line.split(" ").map { it.toInt() }
+        isSafe(numbers)
+    }
+
+    fun part2(input: List<String>): Int = input.count { line ->
+        val numbers = line.split(" ").map { it.toInt() }
+        isSafe(numbers) || canBeMadeSafe(numbers)
+    }
+
+    val testInput = readInput("day2")
+
+    val result = part1(testInput)
+    print("result => ")
+    println(result)
+
+    val result2 = part2(testInput)
+    print("result => ")
+    println(result2)
+}
