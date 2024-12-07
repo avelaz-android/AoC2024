636a9eb8b67dd88ca34ccd3cbfc0c441675245d4
avelaz-android <albertovmo@inditex.com>
2024-12-07 16:36:15 +0100

    Create Day6.kt

---
+++ src/Day6.kt
@@ -0,0 +1,17 @@
+import kotlin.math.abs
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("day6")
+
+    println("result => ${part1(testInput)}")
+    print("result => ${part2(testInput)}")
+}
